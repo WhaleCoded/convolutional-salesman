254ed18f3dc8bcb5bd6eb0298b6e7fd9d4219f53
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2022-04-04 23:18:40 -0600

    Start writing q helper functions

--- q_helper.py
+++ q_helper.py
@@ -1,3 +1,4 @@
+from jinja2 import pass_context
 import numpy as np
 from typing import Tuple, List
 import copy
@@ -6,11 +7,71 @@ import math
 from torch import TensorType
 import torch
 
-IMPOSSIBLE_VALUE = 1000000000000.0
+IMPOSSIBLE_VALUE = 1000.0
+IMPOSSIBLE_THRESHOLD = 500.0
+Q_DISCOUNT_FACTOR = 0.9
 
-def calc_q_actual(model: torch.nn.Module, cost_matrix: torch.Tensor, curr_path: torch.Tensor):
-    with torch.no_grad():
-                
+
+def calculate_q_matrix(
+    model: torch.nn.Module, environment_state: torch.Tensor
+) -> torch.Tensor:
+    cost_matrix, _, previous_move = torch.split(environment_state, 1, dim=1)
+    n_cities = cost_matrix.shape[0]
+    q_actual = torch.full((n_cities, n_cities), fill_value=IMPOSSIBLE_VALUE)
+
+    if any(previous_move):
+        _, prev_target = torch.argmax(previous_move)
+        for next_target in range(n_cities):
+            q_actual[prev_target, next_target] = calculate_q(
+                model, environment_state, prev_target, next_target
+            )
+    else:
+        for next_origin in range(n_cities):
+            for next_target in range(n_cities):
+                q_actual[next_origin, next_target] = calculate_q(
+                    model, environment_state, next_origin, next_target
+                )
+    return q_actual
+
+
+def calculate_q(
+    model: torch.nn.Module,
+    environment_state: torch.Tensor,
+    origin_city: int,
+    target_city: int,
+) -> float:
+    cost_matrix, current_path, previous_move = torch.split(environment_state, 1, dim=1)
+    if cost_matrix[origin_city, target_city] > IMPOSSIBLE_THRESHOLD:
+        return IMPOSSIBLE_VALUE
+    n_cities = cost_matrix.shape[0]
+    edges_left = (n_cities - 1) - torch.sum(current_path)
+    costs = []
+    for _ in range(edges_left):
+        with torch.no_grad():
+            predicted_q = model(environment_state)
+            (origin_city, target_city), cost = get_model_move(
+                predicted_q, environment_state
+            )
+            previous_move = torch.zeros(cost_matrix.shape)
+            previous_move[origin_city][target_city] = 1
+            current_path[origin_city][target_city] = 1
+            environment_state = torch.stack(
+                [cost_matrix, current_path, previous_move], dim=1
+            )
+            costs.append(cost)
+    return q_function(costs)
+
+
+def get_model_move(
+    predicted_q: torch.Tensor, environment_state: torch.Tensor
+) -> Tuple[Tuple[int, int], float]:
+    cost_matrix, current_path, previous_move = torch.split(environment_state, 1, dim=1)
+
+
+def q_function(costs: List[float]) -> float:
+    total_cost = 0
+    for idx, cost in enumerate(costs):
+        total_cost += cost * (Q_DISCOUNT_FACTOR ^ idx)
 
 
 def convert_edge_list_to_matrix(curr_path: Tuple[int, int], num_cities) -> np.ndarray:
@@ -20,52 +81,62 @@ def convert_edge_list_to_matrix(curr_path: Tuple[int, int], num_cities) -> np.nd
 
     return final_matrix
 
+
 def convert_matrix_to_edge_list(path_matrix: np.ndarray) -> Tuple[int, int]:
     edge_list = []
     for i in range(len(path_matrix)):
         for x in range(len(path_matrix)):
             if path_matrix[i][x] == 1:
-                edge_list.append((i,x))
+                edge_list.append((i, x))
 
     return edge_list
 
-def expand_state(curr_matrix: np.ndarray, curr_lower_bound: float, curr_path: List[Tuple[int,int]]) -> List[Tuple[np.ndarray, float, List[Tuple[int,int]]]]:
-		new_states = []	
 
-		#we need to go from this city to the next city
-		if len(curr_path) == 0:
-			next_city_index = 0
-		else:
-			next_city_index = curr_path[len(curr_path) -1 ][1]
-		possible_moves = get_next_possible_locations(curr_matrix, next_city_index)
+def expand_state(
+    curr_matrix: np.ndarray, curr_lower_bound: float, curr_path: List[Tuple[int, int]]
+) -> List[Tuple[np.ndarray, float, List[Tuple[int, int]]]]:
+    new_states = []
 
-		#get possible moves from this city
-		for edge in possible_moves:
-			temp = copy.deepcopy(curr_matrix)
-			temp, additional_cost = expand_on_location(edge, temp)
-			new_path = copy.deepcopy(curr_path)
-			new_path.append(edge)
-			new_states.append((temp, curr_lower_bound + additional_cost, new_path))
+    # we need to go from this city to the next city
+    if len(curr_path) == 0:
+        next_city_index = 0
+    else:
+        next_city_index = curr_path[len(curr_path) - 1][1]
+    possible_moves = get_next_possible_locations(curr_matrix, next_city_index)
 
-		return new_states
+    # get possible moves from this city
+    for edge in possible_moves:
+        temp = copy.deepcopy(curr_matrix)
+        temp, additional_cost = expand_on_location(edge, temp)
+        new_path = copy.deepcopy(curr_path)
+        new_path.append(edge)
+        new_states.append((temp, curr_lower_bound + additional_cost, new_path))
+
+    return new_states
+
 
 # get the coordinates of each zero in the cost matrix
-	#O(n^2) time and O(n) space
-def get_next_possible_locations(curr_matrix: np.ndarray, next_city_index: int) -> List[Tuple[int,int]]:
+# O(n^2) time and O(n) space
+def get_next_possible_locations(
+    curr_matrix: np.ndarray, next_city_index: int
+) -> List[Tuple[int, int]]:
     possible_locations = []
 
-    #O(n^2) time and O(n) space
+    # O(n^2) time and O(n) space
     for x in range(len(curr_matrix)):
         if curr_matrix[next_city_index][x] != IMPOSSIBLE_VALUE:
             possible_locations.append((next_city_index, x))
 
     return possible_locations
 
-#calculates a new state and lower_bound based on a new move
-# time complexity is O(n^2) and space is O(n^2)	
-def expand_on_location(next_move: Tuple[int,int], curr_matrix: np.ndarray) -> Tuple[np.ndarray, float]:
-    #replace row and column of the next_move wiht infinty
-    #O(n)
+
+# calculates a new state and lower_bound based on a new move
+# time complexity is O(n^2) and space is O(n^2)
+def expand_on_location(
+    next_move: Tuple[int, int], curr_matrix: np.ndarray
+) -> Tuple[np.ndarray, float]:
+    # replace row and column of the next_move wiht infinty
+    # O(n)
     temp = copy.deepcopy(curr_matrix)
     move_cost = temp[next_move[0]][next_move[1]]
     temp[next_move[1]][next_move[0]] = IMPOSSIBLE_VALUE
@@ -73,33 +144,36 @@ def expand_on_location(next_move: Tuple[int,int], curr_matrix: np.ndarray) -> Tu
     for i in range(len(curr_matrix)):
         temp[i][next_move[1]] = IMPOSSIBLE_VALUE
 
-    #adjust the matrix
+    # adjust the matrix
     new_cost_matrix, additional_cost = solve_cost_matrix(temp)
     return (new_cost_matrix, additional_cost + move_cost)
 
-#this function makes sure there is a 0 in every column or 
-#row where the whole row is not infinity
-#time complexity is O(n^2) and space is O(n^2)
+
+# this function makes sure there is a 0 in every column or
+# row where the whole row is not infinity
+# time complexity is O(n^2) and space is O(n^2)
 def solve_cost_matrix(curr_matrix: np.ndarray) -> Tuple[np.ndarray, float]:
     additional_cost = 0
 
-    #adjust rows
-    #O(n^2)
+    # adjust rows
+    # O(n^2)
     for i in range(len(curr_matrix)):
-        
+
         curr_min = curr_matrix[i][0]
-        for x in range(len(curr_matrix[i])): 
+        for x in range(len(curr_matrix[i])):
             if curr_matrix[i][x] < curr_min:
                 curr_min = curr_matrix[i][x]
 
-        #if the row is all inf or has a 0 we dont have to adjust
+        # if the row is all inf or has a 0 we dont have to adjust
         if curr_min != IMPOSSIBLE_VALUE and curr_min != 0:
-            #subtract the minimum from each item in the row
-            curr_matrix[i] = [curr_matrix[i][x] - curr_min for x in range(len(curr_matrix[i]))]
+            # subtract the minimum from each item in the row
+            curr_matrix[i] = [
+                curr_matrix[i][x] - curr_min for x in range(len(curr_matrix[i]))
+            ]
             additional_cost += curr_min
 
-    #adjust columns
-    #O(n^2)
+    # adjust columns
+    # O(n^2)
     for i in range(len(curr_matrix[0])):
 
         curr_min = curr_matrix[0][i]
@@ -107,9 +181,9 @@ def solve_cost_matrix(curr_matrix: np.ndarray) -> Tuple[np.ndarray, float]:
             if curr_matrix[x][i] < curr_min:
                 curr_min = curr_matrix[x][i]
 
-        #if the column is all inf or has a 0 we dont have to adjust
+        # if the column is all inf or has a 0 we dont have to adjust
         if curr_min != IMPOSSIBLE_VALUE and curr_min != 0:
-            #subtract the minimum from each item in the column
+            # subtract the minimum from each item in the column
             additional_cost += curr_min
             for x in range(len(curr_matrix)):
                 curr_matrix[x][i] = curr_matrix[x][i] - curr_min
--- train.py
+++ train.py
@@ -13,7 +13,7 @@ NUM_EPOCHS = 10
 NUM_CITIES = 10
 
 model = nn.Sequential(
-    nn.Conv2d(2, 16, 5, padding=2),
+    nn.Conv2d(3, 16, 5, padding=2),
     nn.ReLU(),
     nn.Conv2d(16, 32, 5, padding=2),
     nn.ReLU(),
@@ -27,34 +27,39 @@ model = nn.Sequential(
     nn.ReLU(),
     nn.Conv2d(32, 16, 5, padding=2),
     nn.ReLU(),
-    nn.Conv2d(16, 1, 5, padding=2)
+    nn.Conv2d(16, 1, 5, padding=2),
 )
 # device = "cuda:0"
 # model.to(device=device)
 
-dataset = TSPDataset(length = 500, num_cities= NUM_CITIES)
+dataset = TSPDataset(length=500, num_cities=NUM_CITIES)
 train_set, test_set = dataset.split()
 
-train_loader = DataLoader(dataset=train_set, batch_size = 32)
+train_loader = DataLoader(dataset=train_set, batch_size=32)
 test_loader = DataLoader(dataset=test_set, batch_size=32)
 loss_fn = nn.MSELoss()
-optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-3, weight_decay=.3, betas=(0.95,0.99))
+optimizer = torch.optim.AdamW(
+    model.parameters(), lr=3e-3, weight_decay=0.3, betas=(0.95, 0.99)
+)
 
 
 curr_best_test = torch.inf
 for epoch in range(NUM_EPOCHS):
-    print(f'--- Epoch {epoch} ---')
-    p_bar = tqdm(total = len(train_loader)*(NUM_CITIES-1), desc="Training")
+    print(f"--- Epoch {epoch} ---")
+    p_bar = tqdm(total=len(train_loader) * (NUM_CITIES - 1), desc="Training")
 
     loss_values = []
     for batch in train_loader:
         cost_matrix = batch["data"]
-        path_sf = torch.zeros(cost_matrix.shape)
+        current_path = torch.zeros(cost_matrix.shape)
+        previous_move = torch.zeros(cost_matrix.shape)
 
-        for step in range(NUM_CITIES-1):
-            model_inputs = torch.stack([cost_matrix, path_sf], 1).float()
-            q_pred = model(model_inputs)
-            q_act = q_helper.calc_q_actual(model, cost_matrix, path_sf)
+        for step in range(NUM_CITIES - 1):
+            environment_state = torch.stack(
+                [cost_matrix, current_path, previous_move], dim=1
+            ).float()
+            q_pred = model(environment_state)
+            q_act = q_helper.calculate_q_matrix(model, environment_state)
 
             loss = loss_fn(q_pred, q_act)
             loss_values.append(loss.detach().item())
@@ -62,13 +67,12 @@ for epoch in range(NUM_EPOCHS):
             optimizer.step()
             optimizer.zero_grad()
 
-            cost_matrix, path_sf = q_helper.take_model_step(q_pred, cost_matrix, path_sf)
+            environment_state = q_helper.update_state(q_pred, environment_state)
 
-            p_bar.set_postfix({'average_loss': np.average(loss_values[-20:])})
+            p_bar.set_postfix({"average_loss": np.average(loss_values[-20:])})
             p_bar.update(1)
 
-
-    #test the data
+    # test the data
     abs_loss_values = []
     t_p_bar = tqdm(train_loader, desc="Validation")
     for batch in t_p_bar:
@@ -76,10 +80,11 @@ for epoch in range(NUM_EPOCHS):
             path_cost = q_helper.calculate_model_cost(model, batch["data"])
             greedy_cost = q_helper.calculate_greedy_cost(batch["data"])
             abs_loss_values.append(path_cost - greedy_cost)
-            p_bar.set_postfix({'absolute_loss': np.average(torch.cat(abs_loss_values, 0))})
+            p_bar.set_postfix(
+                {"absolute_loss": np.average(torch.cat(abs_loss_values, 0))}
+            )
 
-    
-    #save the model
+    # save the model
     if np.average(torch.cat(abs_loss_values, 0)) < curr_best_test:
         print("We saved a model")
         curr_best_test = np.average(torch.cat(abs_loss_values, 0))
